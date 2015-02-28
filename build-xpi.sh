67add8a582182fdf1bde273bf416ac4b74431da9
Atul Varma <varmaa@gmail.com>
2015-02-28 16:10:13 -0500

    added build-xpi.sh.

---
+++ build-xpi.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+cfx xpi \
+  --update-link \
+  https://toolness.github.io/twitter-is-dumb/twitter-is-dumb.xpi \
+  --update-url \
+  https://toolness.github.io/twitter-is-dumb/addon/twitter-is-dumb.update.rdf
